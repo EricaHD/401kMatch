2188554c9bbf949e3f3f018a2fb88aec20a5e680
EricaHD <erica.h.dominic@gmail.com>
2024-05-18 19:34:21 -0400

    Convert localStorage utils to TypeScript

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const useLocalStorageState = <T>(key: string, initialValue: T): [T, (newVal: T) => void] => {
+  const [value, setValue] = React.useState<T>(() => getLocalStorage<T>(key) ?? initialValue);
+  const setLocalStorage = (newValue: T) => {
+    if (key != null) localStorage.setItem(key, JSON.stringify(newValue));
+    setValue(newValue);
+  };
+  return [value, setLocalStorage];
+}
+
+const getLocalStorage = <T>(key: string): T | null => {
+  const rawValue = localStorage.getItem(key);
+  if (rawValue == null) return null;
+  try {
+    return JSON.parse(rawValue) as T;
+  } catch {
+    return null;
+  }
+}
