11e4e6b05e3e7db44b2414f9484575afafbd694a
EricaHD <erica.h.dominic@gmail.com>
2024-05-18 19:01:55 -0400

    Convert monetaryCalculations utils to TypeScript

--- src/utils/monetaryCalculations.ts
+++ src/utils/monetaryCalculations.ts
@@ -1,4 +1,4 @@
-export const roundToNearestCent = (value) => {
+export const roundToNearestCent = (value: number): number => {
   return Math.round((value + Number.EPSILON) * 100) / 100;
 }
 
@@ -7,7 +7,7 @@ export const currencyFormatter = new Intl.NumberFormat('en-US', {
   currency: 'USD',
 }).format;
 
-export const twoPercentOfIncome = (incomeArray, stiIdx) => {
+export const twoPercentOfIncome = (incomeArray: number[], stiIdx: number): number => {
   // Round per paycheck (instead of once at the end of the calculation)
   return incomeArray.reduce((accumulator, income) => accumulator + roundToNearestCent(income * 0.02), 0);
 }
