f7951e019c0c39c2ee37a64fa1f4c3ac3b3737c5
EricaHD <erica.h.dominic@gmail.com>
2024-12-30 01:03:00 -0500

    Change style of CumulativeContributionInfo component

--- src/styles/CumulativeContributionInfo.ts
+++ src/styles/CumulativeContributionInfo.ts
@@ -1,7 +1,6 @@
 export default {
   contributionInfo: {
-    background: 'rgba(255, 255, 0, 0.3)',
-    border: '5px dashed #f5ee6c',
+    background: 'rgba(134, 118, 196, 0.3)',
     borderRadius: '10px',
     marginTop: '15px',
     padding: '10px',
