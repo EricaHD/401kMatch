d1660ca5c86930e257e111a349097a96edeba87b
EricaHD <erica.h.dominic@gmail.com>
2026-04-09 21:58:59 -0400

    Fix console error

--- src/styles/SummaryTable.ts
+++ src/styles/SummaryTable.ts
@@ -9,7 +9,7 @@ export default {
   tableRow: {
     height: '60px',
     '&:last-child td, &:last-child th': { border: 0 },
-    '&:first-child, &:last-child': {
+    '&:first-of-type, &:last-of-type': {
       backgroundColor: '#f4f4f4',
     },
   },
